6177da5b7dc683218ea761ae1753d847304572f1
devRahul <rahulpmurali@gmail.com>
2023-10-19 11:40:55 +0100

    setting up react in laravel

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full flex justify-center p-2'>
+        <p className='text-[#FF0000]'>Welcome to Laravel React</p>
+      </div>
+    </>
+  )
+}
+
+export default App
