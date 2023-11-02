628b8ba4ee57b7c71edfcc919bd2639c276c3e62
devRahul <rahulpmurali@gmail.com>
2023-11-02 20:23:21 +0000

    added 1-many relationship for users and courses

--- database/migrations/2023_11_02_202226_create_primary_guardians_table.php
+++ database/migrations/2023_11_02_202226_create_primary_guardians_table.php
@@ -11,19 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('primary_guardians', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-
-
             $table->timestamps();
-
-
-       
         });
     }
 
@@ -32,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('primary_guardians');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(CourseTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+
     }
 }
